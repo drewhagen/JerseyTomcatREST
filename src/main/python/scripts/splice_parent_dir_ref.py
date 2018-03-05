5f57a9839644131fe9b78c793c3981ca658b41c5
INFINITECAMPUS\drew.hagen <drew.hagen@infinitecampus.com>
2018-03-05 13:15:29 -0600

    Removing python scripts on the master branch.

--- src/main/python/scripts/splice_parent_dir_ref.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def eachline(tf):  #tf -> txt file, cf -> csv file
-    if os.path.isfile(tf):
-        thisfile = open(tf, 'r')
-        for eachline in thisfile:
-            print eachline[2:]
-    else:
-        print "not a file"
